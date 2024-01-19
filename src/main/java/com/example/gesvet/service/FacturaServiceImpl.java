81dce4ef2edbd5b7004e46e8afee9a3d675b3ad4
Maleja_Giraldo <malejagiraldoc@gmail.com>
2024-01-19 08:36:08 -0500

    Se creó método para generar número de factura

--- src/main/java/com/example/gesvet/service/FacturaServiceImpl.java
+++ src/main/java/com/example/gesvet/service/FacturaServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.gesvet.service;
 
 import com.example.gesvet.models.Factura;
 import com.example.gesvet.repository.IFacturaRepository;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,49 @@ public class FacturaServiceImpl implements IFacturaService{
     @Override
     public Factura save(Factura factura) {
         return facturaRepository.save(factura);
+    }
+
+    @Override
+    public List<Factura> findAll() {
+        return facturaRepository.findAll();
+    }
+    
+    public String generarNumFactura(){
+        
+        //Se crean dos variables. Uno para int y otro para string
+        int numero = 0;
+        String numeroConcatenado = "";
+        
+        //Se crea lista para almacenar facturas
+        List<Factura> facturas = findAll();
+        
+        //Se crea lista para almacenar números
+        List<Integer> numeros = new ArrayList<Integer>();
+        
+        //Método para recorrer lista factura y adicionar el numero de cada una en la lista numeros, haciendo el debido parseo
+        facturas.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+        
+        if(facturas.isEmpty()){
+            numero=1;
+        }else{
+            //Método para extraer el número mayor de factura y almacenarlo en la lista números
+            numero = numeros.stream().max(Integer::compare).get();
+            
+            //El último número se incrementa el valor
+            numero++;
+        }
+        
+        if (numero<10) { //0000001000
+			numeroConcatenado="000000000"+String.valueOf(numero);
+		}else if(numero<100) {
+			numeroConcatenado="00000000"+String.valueOf(numero);
+		}else if(numero<1000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}else if(numero<10000) {
+			numeroConcatenado="0000000"+String.valueOf(numero);
+		}
+        
+        return numeroConcatenado;
     }
     
 }
--- src/main/java/com/example/gesvet/service/IFacturaService.java
+++ src/main/java/com/example/gesvet/service/IFacturaService.java
@@ -2,10 +2,12 @@
 package com.example.gesvet.service;
 
 import com.example.gesvet.models.Factura;
+import java.util.List;
 
 
 public interface IFacturaService {
     
+    List<Factura> findAll();
     Factura save(Factura factura);
     
 }
